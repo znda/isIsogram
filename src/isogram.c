ef843b0689dcbb1fb1cc11b1dd8597074bb5b808
Mathias Rieder <mathias.rieder@gmail.com>
2018-04-24 20:48:53 +0200

    implementation...

--- src/isogram.c
+++ src/isogram.c
@@ -1,6 +1,33 @@
 #include "isogram.h"
+#include <string.h>
+#include <stdio.h>
+
+char toLowerCase(char c){
+  if (c >= 'A' && c <= 'Z'){
+    return c + ('a' - 'A');
+  }
+  return c;
+}
+
+bool isChar(char c){
+  return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <='z');
+}
+
 
 bool is_isogram(const char phrase[]){
+  bool occurence[256];
 
-  return phrase != 0;
+  for (size_t i = 0; i < 256; i++) {
+    occurence[i] = false;
+  }
+
+  for (size_t i = 0; i < strlen(phrase); i++) {
+    int charIndex =(int) toLowerCase(phrase[i]);
+    if(isChar(phrase[i]) && occurence[charIndex] == true){
+      return false;
+    }
+    occurence[charIndex] = true;
+  }
+
+  return true;
 }
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -7,13 +7,52 @@ void test_empty_string(void)
    TEST_ASSERT_TRUE(is_isogram(""));
 }
 
+void test_single_characters(void)
+{
+   TEST_ASSERT_TRUE(is_isogram("a"));
+   TEST_ASSERT_TRUE(is_isogram("B"));
+   TEST_ASSERT_TRUE(is_isogram("z"));
+}
+
+void test_multiple_multiple_same_characters(void)
+{
+   TEST_ASSERT_FALSE(is_isogram("aa"));
+   TEST_ASSERT_FALSE(is_isogram("BB"));
+   TEST_ASSERT_FALSE(is_isogram("zz"));
+}
+
+void test_isograms_lowercase(void)
+{
+  TEST_ASSERT_TRUE(is_isogram("asdfjkl"));
+  TEST_ASSERT_TRUE(is_isogram("sa"));
+  TEST_ASSERT_TRUE(is_isogram("isogram"));
+}
+
+void test_isograms_different_casings(void)
+{
+  TEST_ASSERT_FALSE(is_isogram("aA"));
+}
+
+void test_isograms_ignore_specialchars(void)
+{
+  TEST_ASSERT_TRUE(is_isogram("isogram!!"));
+  TEST_ASSERT_TRUE(is_isogram("isogram&&"));
+  TEST_ASSERT_TRUE(is_isogram("isogram???"));
+  TEST_ASSERT_TRUE(is_isogram("isogram   "));
+}
+
 int main(void)
 {
    UnityBegin("test/test_isogram.c");
 
    RUN_TEST(test_empty_string);
-
+   RUN_TEST(test_single_characters);
+   RUN_TEST(test_multiple_multiple_same_characters);
+   RUN_TEST(test_isograms_lowercase);
+   RUN_TEST(test_isograms_different_casings);
+   RUN_TEST(test_isograms_ignore_specialchars);
 
    UnityEnd();
+
    return 0;
 }
