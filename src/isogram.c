35673875caf8d45becad7a5e5ecf8cda00b9c314
znda <david.zengerle@gmx.at>
2020-05-06 10:56:52 +0000

    <added is_isogram>

--- src/isogram.c
+++ src/isogram.c
@@ -1,7 +1,33 @@
 #include "isogram.h"
-
+#include "string.h"
 
 bool is_isogram(const char phrase[]){
+    int len = strlen(phrase);
+    for (int i=0; i<len; ++i){
+        char currentChar = phrase[i];
+        if(currentChar == ' '){
+            continue;
+        }
 
-    return false;
+        int occurences = 0;
+        for(int j=0; j<len; ++j){
+            char checkChar = phrase[j];
+            if(checkChar == ' '){
+            continue;
+            }
+            if (currentChar >= 'A' && currentChar <= 'Z'){
+            currentChar = currentChar + ('a'-'A');
+            }
+            if (checkChar >= 'A' && checkChar <= 'Z'){
+            checkChar = checkChar + ('a'-'A');
+            }
+            if (checkChar == currentChar){
+                ++occurences;
+            }
+        }
+        if(occurences > 1){
+            return false;
+        }
+    }
+    return true;
 }
